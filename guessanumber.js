b0fcc7b6c3fd12f98679f6985765a8e3a41ef25d
meduardasf <meduardasferreira@outlook.com>
2019-12-11 22:50:51 +0100

    mais js

--- guessanumber.js
+++ guessanumber.js
@@ -2,11 +2,15 @@ var number = 4;
 
 var guess = prompt("guess a number!");
 
-if (Number(guess) === number) {
+if(Number(guess) === number) {
     alert("you got it right!!!!")
 }
 
+else if(Number(guess) > number) {
+    alert("too high. try again")
+}
+
 else {
-    alert("WROOONG")
+    alert("too low. try again")
 }
 
---
+++ javaloopsex.js
@@ -0,0 +1,31 @@
+var count = -10;
+while (count < 20){
+      console.log("count is " + count);
+      count++;
+}
+
+
+
+var count = 10;
+while (count <= 40){
+   if (count % 2 === 0){
+       console.log(count);
+   }
+  count++
+}
+
+var count = 300;
+while (count <= 333){
+   if (count % 2 !== 0){
+       console.log(count);
+   }
+  count++
+}
+
+var count = 5;
+while (count < 50){
+    if(count % 5 === 0 && count % 3 === 0){
+        console.log(count);
+    }
+    count++
+}
